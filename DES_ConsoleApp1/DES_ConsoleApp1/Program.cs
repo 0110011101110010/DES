38dfd923c9d1af1dd474d57197e5204102239ef1
0110011101110010 <51711161+0110011101110010@users.noreply.github.com>
2021-02-24 10:07:44 +0200

    Pavyzdys
    1. pavyzdys iš https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.des.create?view=net-5.0#System_Security_Cryptography_DES_Create

--- DES_ConsoleApp1/DES_ConsoleApp1/Program.cs
+++ DES_ConsoleApp1/DES_ConsoleApp1/Program.cs
@@ -69,93 +69,101 @@ namespace DES_ConsoleApp1
 
                 // Create a string to encrypt.
                 string sData = "Here is some data to encrypt.";
+                string FileName = "CText.txt";
 
-                // Encrypt the string to an in-memory buffer.
-                byte[] Data = EncryptTextToMemory(sData, DESalg.Key, DESalg.IV);
+                // Encrypt text to a file using the file name, key, and IV.
+                EncryptTextToFile(sData, FileName, DESalg.Key, DESalg.IV);
 
-                // Decrypt the buffer back to a string.
-                string Final = DecryptTextFromMemory(Data, DESalg.Key, DESalg.IV);
+                // Decrypt the text from a file using the file name, key, and IV.
+                string Final = DecryptTextFromFile(FileName, DESalg.Key, DESalg.IV);
 
                 // Display the decrypted string to the console.
                 Console.WriteLine(Final);
             }
-            catch (Exception exc)
+            catch (Exception e)
             {
-                Console.WriteLine(exc.Message);
+                Console.WriteLine(e.Message);
             }
         }
-        public static byte[] EncryptTextToMemory(string Data, byte[] Key, byte[] IV)
+        public static void EncryptTextToFile(String Data, String FileName, byte[] Key, byte[] IV)
         {
             try
             {
-                // Create a MemoryStream.
-                MemoryStream mStream = new MemoryStream();
+                // Create or open the specified file.
+                FileStream fStream = File.Open(FileName, FileMode.OpenOrCreate);
 
                 // Create a new DES object.
                 DES DESalg = DES.Create();
 
-                // Create a CryptoStream using the MemoryStream
+                // Create a CryptoStream using the FileStream
                 // and the passed key and initialization vector (IV).
-                CryptoStream cStream = new CryptoStream(mStream,
+                CryptoStream cStream = new CryptoStream(fStream,
                     DESalg.CreateEncryptor(Key, IV),
                     CryptoStreamMode.Write);
 
-                // Convert the passed string to a byte array.
-                byte[] toEncrypt = new ASCIIEncoding().GetBytes(Data);
+                // Create a StreamWriter using the CryptoStream.
+                StreamWriter sWriter = new StreamWriter(cStream);
 
-                // Write the byte array to the crypto stream and flush it.
-                cStream.Write(toEncrypt, 0, toEncrypt.Length);
-                cStream.FlushFinalBlock();
+                // Write the data to the stream
+                // to encrypt it.
+                sWriter.WriteLine(Data);
 
-                // Get an array of bytes from the
-                // MemoryStream that holds the
-                // encrypted data.
-                byte[] ret = mStream.ToArray();
-
-                // Close the streams.
+                // Close the streams and
+                // close the file.
+                sWriter.Close();
                 cStream.Close();
-                mStream.Close();
+                fStream.Close();
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine("A Cryptographic error occurred: {0}", e.Message);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine("A file error occurred: {0}", e.Message);
+            }
+        }
 
-                // Return the encrypted buffer.
-                return ret;
+        public static string DecryptTextFromFile(String FileName, byte[] Key, byte[] IV)
+        {
+            try
+            {
+                // Create or open the specified file.
+                FileStream fStream = File.Open(FileName, FileMode.OpenOrCreate);
+
+                // Create a new DES object.
+                DES DESalg = DES.Create();
+
+                // Create a CryptoStream using the FileStream
+                // and the passed key and initialization vector (IV).
+                CryptoStream cStream = new CryptoStream(fStream,
+                    DESalg.CreateDecryptor(Key, IV),
+                    CryptoStreamMode.Read);
+
+                // Create a StreamReader using the CryptoStream.
+                StreamReader sReader = new StreamReader(cStream);
+
+                // Read the data from the stream
+                // to decrypt it.
+                string val = sReader.ReadLine();
+
+                // Close the streams and
+                // close the file.
+                sReader.Close();
+                cStream.Close();
+                fStream.Close();
+
+                // Return the string.
+                return val;
             }
             catch (CryptographicException e)
             {
                 Console.WriteLine("A Cryptographic error occurred: {0}", e.Message);
                 return null;
             }
-        }
-
-        public static string DecryptTextFromMemory(byte[] Data, byte[] Key, byte[] IV)
-        {
-            try
+            catch (UnauthorizedAccessException e)
             {
-                // Create a new MemoryStream using the passed
-                // array of encrypted data.
-                MemoryStream msDecrypt = new MemoryStream(Data);
-
-                // Create a new DES object.
-                DES DESalg = DES.Create();
-
-                // Create a CryptoStream using the MemoryStream
-                // and the passed key and initialization vector (IV).
-                CryptoStream csDecrypt = new CryptoStream(msDecrypt,
-                    DESalg.CreateDecryptor(Key, IV),
-                    CryptoStreamMode.Read);
-
-                // Create buffer to hold the decrypted data.
-                byte[] fromEncrypt = new byte[Data.Length];
-
-                // Read the decrypted data out of the crypto stream
-                // and place it into the temporary buffer.
-                csDecrypt.Read(fromEncrypt, 0, fromEncrypt.Length);
-
-                //Convert the buffer into a string and return it.
-                return new ASCIIEncoding().GetString(fromEncrypt);
-            }
-            catch (CryptographicException e)
-            {
-                Console.WriteLine("A Cryptographic error occurred: {0}", e.Message);
+                Console.WriteLine("A file error occurred: {0}", e.Message);
                 return null;
             }
         }
